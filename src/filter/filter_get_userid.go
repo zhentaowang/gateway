05083bbecbd7b5610becb653298a8d315566eb36
immortal <huangmingle@zhiweicloud.com>
2017-09-02 14:17:53 +0800

    filter认证失败退出

--- src/filter/filter_get_userid.go
+++ src/filter/filter_get_userid.go
@@ -3,6 +3,7 @@ package filter
 import (
     "net/http"
     "gateway/src/util"
+    "github.com/juju/errors"
 )
 
 type VisitOauth struct {
@@ -22,6 +23,10 @@ func (v VisitOauth) Pre(c Context) (statusCode int, err error) {
     ok , err := util.CheckToken(c.GetProxyOuterRequest(),c.GetProxyResponse())
     if ok {
         return http.StatusOK,nil
+    }
+
+    if err == nil {
+        err = errors.New("token认证失败")
     }
 
     return http.StatusForbidden,err
